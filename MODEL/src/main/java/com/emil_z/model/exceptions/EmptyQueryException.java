ad4cec0676eff29c059c7a7d0f780224acf5dbe5
Emil Zalessky <emilzal2016@gmail.com>
2025-05-17 17:13:22 +0300

    refactor: enhance exception classes with detailed documentation and constructors for better clarity

--- MODEL/src/main/java/com/emil_z/model/exceptions/EmptyQueryException.java
+++ MODEL/src/main/java/com/emil_z/model/exceptions/EmptyQueryException.java
@@ -2,19 +2,47 @@ package com.emil_z.model.exceptions;
 
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
+/**
+ * Custom exception class that extends FirebaseFirestoreException.
+ * This exception is used to indicate that no games are available in the queue.
+ */
 public class EmptyQueryException extends FirebaseFirestoreException {
+	/**
+	 * Default constructor that initializes the exception with a default message
+	 * and a NOT_FOUND error code.
+	 */
 	public EmptyQueryException() {
 		this("No games in queue", Code.NOT_FOUND);
 	}
 
+	/**
+	 * Constructor that initializes the exception with a default message,
+	 * a NOT_FOUND error code, and a specified cause.
+	 *
+	 * @param cause The throwable cause of the exception.
+	 */
 	public EmptyQueryException(Throwable cause) {
 		this("No games in queue", Code.NOT_FOUND, cause);
 	}
 
+	/**
+	 * Constructor that initializes the exception with a custom message and error code.
+	 *
+	 * @param message The custom error message.
+	 * @param code    The error code associated with the exception.
+	 */
 	public EmptyQueryException(String message, Code code) {
 		super(message, code);
 	}
 
+	/**
+	 * Constructor that initializes the exception with a custom message, error code,
+	 * and a specified cause.
+	 *
+	 * @param message The custom error message.
+	 * @param code    The error code associated with the exception.
+	 * @param cause   The throwable cause of the exception.
+	 */
 	public EmptyQueryException(String message, Code code, Throwable cause) {
 		super(message, code, cause);
 	}
--- MODEL/src/main/java/com/emil_z/model/exceptions/GameFullException.java
+++ MODEL/src/main/java/com/emil_z/model/exceptions/GameFullException.java
@@ -1,19 +1,41 @@
 package com.emil_z.model.exceptions;
 
+/**
+ * Exception thrown to indicate that a game is already full and cannot accept more players.
+ */
 public class GameFullException extends Exception {
-    public GameFullException() {
-        super("Game is full");
-    }
+	/**
+	 * Constructs a new GameFullException with a default message.
+	 */
+	public GameFullException() {
+		super("Game is full");
+	}
 
-    public GameFullException(String message) {
-        super(message);
-    }
+	/**
+	 * Constructs a new GameFullException with the specified detail message.
+	 *
+	 * @param message the detail message.
+	 */
+	public GameFullException(String message) {
+		super(message);
+	}
 
-    public GameFullException(String message, Throwable cause) {
-        super(message, cause);
-    }
+	/**
+	 * Constructs a new GameFullException with the specified detail message and cause.
+	 *
+	 * @param message the detail message.
+	 * @param cause   the cause of the exception.
+	 */
+	public GameFullException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
-    public GameFullException(Throwable cause) {
-        super(cause);
-    }
+	/**
+	 * Constructs a new GameFullException with the specified cause.
+	 *
+	 * @param cause the cause of the exception.
+	 */
+	public GameFullException(Throwable cause) {
+		super(cause);
+	}
 }
